88febf048704afde008dea75b3bb18e41a3d90ce
Olimjonnn <olimjonolimjonov3@gmail.com>
2023-06-23 17:57:54 +0500

    third commit

--- vacancy/views.py
+++ vacancy/views.py
@@ -51,8 +51,7 @@ class VacancyView(generics.ListCreateAPIView):
     queryset = Vacancy.objects.all()
     serializer_class = VacancyDetailSerializers
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['category__name', 'city', 'title', 'worker_experience']
+
 
     def create(self, request):
         serializer = self.serializer_class(data=request.data)
@@ -61,23 +60,23 @@ class VacancyView(generics.ListCreateAPIView):
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
     
-    # def get_queryset(self):
-    #     vacancy = super().get_queryset()
-    #     filter_field = self.request.GET.get("filter_field")
-    #     filtering = Q()
-    #     if filter_field:
-    #         filtering = Q(category__name__icontains=filter_field) | Q(title__icontains=filter_field) | Q(city__icontains=filter_field) |  Q(worker_experience__icontains=filter_field)
-    #     queryset = vacancy.filter(filtering)
-    #     print(queryset)
-    #     return queryset
+    def get_queryset(self):
+        vacancy = super().get_queryset()
+        filter_field = self.request.GET.get("filter_field")
+        filtering = Q()
+        if filter_field:
+            filtering = Q(category__name__icontains=filter_field) | Q(title__icontains=filter_field) | Q(city__icontains=filter_field) |  Q(worker_experience__icontains=filter_field)
+        queryset = vacancy.filter(filtering)
+        print(queryset)
+        return queryset
         
-    # def list(self, request, *args, **kwargs):
-    #         queryset = self.get_queryset()
-    #         if queryset:
-    #             serializer = self.serializer_class(queryset, many=True).data
-
-    #             return Response({'success': True, 'data': serializer}, status=200)
-    #         return Response({'success': False, 'data': "Error"}, status=404)
+    def list(self, request, *args, **kwargs):
+            queryset = self.get_queryset()
+            if queryset:
+                serializer = self.serializer_class(queryset, many=True).data
+                count = queryset.count()
+                return Response({'success': True, 'count':count, 'data': serializer}, status=200)
+            return Response({'success': False, 'data': "Vacancy matching query does not exist"}, status=404)
 
 
 class RelatedVacancy(generics.RetrieveAPIView):
@@ -88,7 +87,7 @@ class RelatedVacancy(generics.RetrieveAPIView):
     def retrieve(self, request, pk):
         vacancy = Vacancy.objects.get(id=pk)
         ser_vac = self.get_serializer(vacancy).data
-        related = Vacancy.objects.filter(category__id=vacancy.category.id)
+        related = Vacancy.objects.filter(category__id=vacancy.category.id)[0:2]
         ser_rel = self.get_serializer(related, many=True).data
         return Response({"status":True, "Vacancy":ser_vac, "Related vacancies":ser_rel})
     
